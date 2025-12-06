5d7f9f25a53e19c08444f85cf13b209019b00aac
Katha-BP <katharine815@hotmail.com>
2025-12-06 16:48:11 +0000

    test all media conditions

--- submit.sh
+++ submit.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
-#SBATCH --job-name=geno_altered
-#SBATCH --partition=compute # mlcnu is the partition with gpu
+#!/bin/bash
+#SBATCH --job-name=all_conditions
+#SBATCH --partition=compute
 #SBATCH --time=3-00:00:00
 #SBATCH --chdir=/user/home/il22158
 #SBATCH --account=emat024603
-#SBATCH --output=slurm_logs/geno_altered.%j.out
-#SBATCH --mem=50G
-#SBATCH --cpus-per-task=3
+#SBATCH --output=slurm_logs/all_conditions.%j.out
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=16
 
 cd /user/work/il22158/vEcoli
 source .venv/bin/activate
-echo "Running test script..."
-python runscripts/workflow.py --config configs/two_generations.json
+echo "Running all media conditions..."
+python runscripts/workflow.py --config configs/N_all_media_conditions.json
