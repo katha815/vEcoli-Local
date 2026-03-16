b8f69a88b84d4d64700999ad69038550d2a00f4b
Katha-BP <katharine815@hotmail.com>
2026-03-16 12:01:10 +0000

    gen_3 colony simulation submitted

--- submit.sh
+++ submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=gowth_r_441
+#SBATCH --job-name=colony_3_gen
 #SBATCH --partition=compute
 #SBATCH --time=3-00:00:00
 #SBATCH --chdir=/user/home/il22158
 #SBATCH --account=emat024603
-#SBATCH --output=slurm_logs/growth_rate_441.%j.out
-#SBATCH --mem=50G
-#SBATCH --cpus-per-task=10
+#SBATCH --output=slurm_logs/colony_3_gen.%j.out
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=24
 
 # === ENVIRONMENT SETUP ===
 cd /user/work/il22158/vEcoli
@@ -31,12 +31,12 @@ echo "Snapshot for job ${SLURM_JOB_ID}"
 # echo "Rerun 441 gene knockouts with downsampled time step..."
 # python runscripts/workflow.py --config configs/N_gene_ko_441imported_2seeds.json
 
-echo "Extracting growth rates from 441 single knockouts..."
-python reading/growth_rate_extract.py --all --projects gene_ko_441imported_2seeds --suffix 441_KOs_seed100 --lineage-seed 100 --save-timeseries
-python reading/growth_rate_extract.py --all --projects gene_ko_441imported_2seeds --suffix 441_KOs_seed101 --lineage-seed 101 --save-timeseries
+# echo "Extracting growth rates from 441 single knockouts..."
+# python reading/growth_rate_extract.py --all --projects gene_ko_441imported_2seeds --suffix 441_KOs_seed100 --lineage-seed 100 --save-timeseries
+# python reading/growth_rate_extract.py --all --projects gene_ko_441imported_2seeds --suffix 441_KOs_seed101 --lineage-seed 101 --save-timeseries
 
 # echo "Performing functional gene analysis..."
 # python reading/functional_gene_analysis.py  
 
-# echo "Running colony simulation starting from the saved 2-gen results..."
-# python ecoli/experiments/ecoli_engine_process.py --config configs/colony_baseline_test2.json
+echo "Running colony simulation from 2nd to 3rd generation..."
+python ecoli/experiments/ecoli_engine_process.py --config configs/colony_baseline_3_gen.json
