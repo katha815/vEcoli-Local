1fd8b5bc79b94a911e81012215177d64f9697af9
Katha-BP <katharine815@hotmail.com>
2026-08-06 16:27:17 +0100

    extended succinate simulation submitted

--- submit.sh
+++ submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=gene_ko_40trial_acetate
+#SBATCH --job-name=gene_ko_40trial_succinate_extended
 #SBATCH --partition=compute
 #SBATCH --time=3-00:00:00
 #SBATCH --chdir=/user/home/il22158
 #SBATCH --account=emat024603
-#SBATCH --output=slurm_logs/gene_ko_40trial_acetate.%j.out
-#SBATCH --mem=100G
-#SBATCH --cpus-per-task=20
+#SBATCH --output=/user/home/il22158/work/slurm_logs/gene_ko_40trial_succinate_extended.%j.out
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=24
 
 # == Work directory setup ==
 WORK_DIR="/user/home/il22158/work/vEcoli"
@@ -34,11 +34,11 @@ module list       # Print loaded modules
 # python reading/downsample_history.py --dir /user/home/il22158/work/vEcoli/out/gene_ko_metabolic_seed100/history --n 20 
 # # change total number of samples to 1/n
 
-# echo "Re-run for 40 trial gene knockout for 16 generations with acetate conditions..."
+echo "Re-run for 40 trial gene knockout for 24 generations with succinate conditions..."
 # # python runscripts/workflow.py --config configs/N_gene_ko_40trial_seed100_basal.json
 # # python runscripts/workflow.py --config configs/N_gene_ko_40trial_seed100_aa.json
 # python runscripts/workflow.py --config configs/N_gene_ko_40trial_seed100_acetate.json
-# python runscripts/workflow.py --config configs/N_gene_ko_40trial_seed100_succinate.json
+python runscripts/workflow.py --config configs/N_gene_ko_40trial_seed100_succinate.json
 # python runscripts/workflow.py --config configs/N_gene_ko_40trial_seed100_no_ox.json
 
 # Resume workflow for previous job ended because  if time limit.
